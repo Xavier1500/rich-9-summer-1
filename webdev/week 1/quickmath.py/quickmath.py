8c9d6cfba9e85cda35953e30210e91c0c2d52432
Xavier Donovan <xavier.donovan@hg.hiddengeniusproject.org>
2025-06-24 14:19:22 -0700

    quick math done

---
+++ webdev/week 1/quickmath.py/quickmath.py
@@ -0,0 +1,9 @@
+#1 delare and sign two variables 
+#2 delare and assign 3rd variable that is the sum of the first 2
+num1 = 4
+num2 = 6
+
+num3= num1 + num2
+
+print(num3)
+#3 print the 3rd variable to the terminal 
